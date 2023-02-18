c8b092b60a609a3a11aab1a675fbd1acd160132a
maslenia mubarrat <maslenia@gmail.com>
2023-02-18 23:02:21 +0600

    first commit

---
+++ first.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+
+void main() {
+  int a = 5;
+  int b = 10;
+  if (a == b) {
+    print("a and b is equal");
+  } else if (a < b) {
+    print("a is less then B");
+  } else {
+    print("a is greater then B");
+  }
+}
