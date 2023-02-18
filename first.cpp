c68313c9065866f83c0e34104578210a8826f939
maslenia mubarrat <maslenia@gmail.com>
2023-02-18 23:15:13 +0600

    third commit

--- first.cpp
+++ first.cpp
@@ -8,6 +8,17 @@ int main(){
 } 
 #include<stdio.h>
 #include<conio.h>
+
+int main(){
+   
+   int a, b;
+   a = 4, b = 10;
+   
+} 
+
+#include<stdio.h>
+#include<conio.h>
+
 int main(){
    
    int a, b;
